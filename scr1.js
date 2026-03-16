73167eeb0dd6e68b108fa138487d8c4c65166c25
JanGomez <jan.estebanrg@gmail.com>
2026-03-16 13:47:52 -0600

    Add files via upload

--- scr1.js
+++ scr1.js
@@ -1,229 +1,40 @@
 // pagina del admin
 
-const clses = {
-    //pabellon 1
-  cls1: false,
-  cls2: false,
-  cls3: false,
-  cls4: false,
-  cls5: false,
-  cls6: false,
-//pabellon 2
-  cls19: false,
-  lab1: false,
-  lab4: false,
-  lab5: false,
-//pabellon 3
-  cls7: false,
-  cls8: false,
-  cls9: false,
-  cls10: false,
-  cls12: false,
-//pabellon 4
-  cls13: false,
-  cls15: false,
-  cls16: false,
-  cls17: false,
-  cls18: false,
-//area tecnica
-  wsh1: false,
-  wsh2: false,
-  wsh3: false,
-  wsh4: false,
-  wsh6: false,
-  wsh7: false,
-  wsh8: false,
-  wsh9: false,
-  wsh10: false
-}
-
+let clses = JSON.parse(localStorage.getItem("cls")) || {
+  cls1:false, cls2:false, cls3:false, cls4:false, cls5:false, cls6:false,
+  cls19:false, lab1:false, lab4:false, lab5:false,
+  cls7:false, cls8:false, cls9:false, cls10:false, cls12:false,
+  cls13:false, cls15:false, cls16:false, cls17:false, cls18:false,
+  wsh1:false, wsh2:false, wsh3:false, wsh4:false, wsh6:false,
+  wsh7:false, wsh8:false, wsh9:false, wsh10:false
+};
 
 function goCheck(){
     window.location.href = "index.html";
 }
 
-function changer(room){
-    const usJSON = localStorage.getItem("user");
-    const user = JSON.parse(usJSON);
-
-    switch(room){
-        //pabellon 1
-        case 1:
-            clses.cls1 = !clses.cls1;
-            console.log(clses.cls1);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-        case 2:
-            clses.cls2 = !clses.cls2;
-            console.log(clses.cls2);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-        case 3:
-            clses.cls3 = !clses.cls3;
-            console.log(clses.cls3);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-        case 4:
-            clses.cls4 = !clses.cls4;
-            console.log(clses.cls4);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-        case 5: 
-            clses.cls5 = !clses.cls5;
-            console.log(clses.cls5);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 6:
-            clses.cls6 = !clses.cls6;
-            console.log(clses.cls6);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        //pabellon2
-
-        case 7:
-            clses.cls19 = !clses.cls19;
-            console.log(clses.cls19);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-        case 8:
-            clses.lab1 = !clses.lab1;
-            console.log(clses.lab1);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 9:
-            clses.lab4 = !clses.lab4;
-            console.log(clses.lab4);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 10:
-            clses.lab5 = !clses.lab5;
-            console.log(clses.lab5);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        //pabellon3
-
-        case 11:
-            clses.cls7 = !clses.cls7;
-            console.log(clses.cls7);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 12:
-            clses.cls8 = !clses.cls8;
-            console.log(clses.cls8);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 13:
-            clses.cls9 = !clses.cls9;
-            console.log(clses.cls9);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 14:
-            clses.cls10 = !clses.cls10;
-            console.log(clses.cls10);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 15:
-            clses.cls12 = !clses.cls12;
-            console.log(clses.cls12);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        //pabellon4
-
-        case 16:
-            clses.cls13 = !clses.cls13;
-            console.log(clses.cls13);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 17:
-            clses.cls15 = !clses.cls15;
-            console.log(clses.cls15);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 18:
-            clses.cls16 = !clses.cls16;
-            console.log(clses.cls16);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 19:
-            clses.cls17 = !clses.cls17;
-            console.log(clses.cls17);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 20:
-            clses.cls18 = !clses.cls18;
-            console.log(clses.cls18);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        //area tecnica
-
-        case 21:
-            clses.wsh1 = !clses.wsh1;
-            console.log(clses.wsh1);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 22:
-            clses.wsh2 = !clses.wsh2;
-            console.log(clses.wsh2);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 23:
-            clses.wsh3 = !clses.wsh3;
-            console.log(clses.wsh3);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 24:
-            clses.wsh4 = !clses.wsh4;
-            console.log(clses.wsh4);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 25:
-            clses.wsh6 = !clses.wsh6;
-            console.log(clses.wsh6);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 26:
-            clses.wsh7 = !clses.wsh7;
-            console.log(clses.wsh7);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 27:
-            clses.wsh8 = !clses.wsh8;
-            console.log(clses.wsh8);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 28:
-            clses.wsh9 = !clses.wsh9;
-            console.log(clses.wsh9);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-
-        case 29:
-            clses.wsh10 = !clses.wsh10;
-            console.log(clses.wsh10);
-            localStorage.setItem("cls", JSON.stringify(clses));
-        break;
-    }
+function save(){
+    localStorage.setItem("cls", JSON.stringify(clses));
 }
 
+function changer(room){
+
+    const map = {
+        1:"cls1", 2:"cls2", 3:"cls3", 4:"cls4", 5:"cls5", 6:"cls6",
+        7:"cls19", 8:"lab1", 9:"lab4", 10:"lab5",
+        11:"cls7", 12:"cls8", 13:"cls9", 14:"cls10", 15:"cls12",
+        16:"cls13", 17:"cls15", 18:"cls16", 19:"cls17", 20:"cls18",
+        21:"wsh1", 22:"wsh2", 23:"wsh3", 24:"wsh4",
+        25:"wsh6", 26:"wsh7", 27:"wsh8", 28:"wsh9", 29:"wsh10"
+    };
+
+    const key = map[room];
+
+    if(!key) return;
+
+    clses[key] = !clses[key];
+
+    console.log(key, clses[key]);
+
+    save();
+}
--- scr2.js
+++ scr2.js
@@ -1,22 +1,30 @@
-// pagina de los usuarios
-
 function goCheck(){
     window.location.href = "test2.html";
 }
 
-
-
 function checker(room){
 
-    const clJSON = localStorage.getItem("cls");
-    const clses = JSON.parse(clJSON);
-   
+    const clses = JSON.parse(localStorage.getItem("cls"));
 
+    if(!clses){
+        document.getElementById("check").textContent = "Sin datos";
+        return;
+    }
 
-    
-    if (clses.cls1) {
+    const map = {
+        1:"cls1", 2:"cls2", 3:"cls3", 4:"cls4", 5:"cls5", 6:"cls6",
+        7:"cls19", 8:"lab1", 9:"lab4", 10:"lab5",
+        11:"cls7", 12:"cls8", 13:"cls9", 14:"cls10", 15:"cls12",
+        16:"cls13", 17:"cls15", 18:"cls16", 19:"cls17", 20:"cls18",
+        21:"wsh1", 22:"wsh2", 23:"wsh3", 24:"wsh4",
+        25:"wsh6", 26:"wsh7", 27:"wsh8", 28:"wsh9", 29:"wsh10"
+    };
+
+    const key = map[room];
+
+    if(clses[key]){
         document.getElementById("check").textContent = "Disponible";
-    } else{
-        document.getElementById("check").textContent = "Ocupada";  
+    }else{
+        document.getElementById("check").textContent = "Ocupada";
     }
 }
